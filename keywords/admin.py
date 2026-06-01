f6f99ff29d234793f939cd020fa31ca3e8d643a6
MUHAMMAD ATIF REHMAN <rehmanatif682@gmail.com>
2026-06-01 12:46:29 +0500

    Add easy approve action

--- keywords/admin.py
+++ keywords/admin.py
@@ -6,6 +6,12 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'email', 'is_approved', 'is_premium', 'is_staff')
     list_editable = ('is_approved', 'is_premium')
     list_filter = ('is_approved', 'is_premium', 'is_staff')
+    actions = ['approve_users']
+
+    @admin.action(description='Approve selected users')
+    def approve_users(self, request, queryset):
+        updated = queryset.update(is_approved=True)
+        self.message_user(request, f"Successfully approved {updated} users.")
 
 @admin.register(KeywordSearch)
 class KeywordSearchAdmin(admin.ModelAdmin):
